ef2d77279e59e480175a62624910fbfa897dd93c
Roman <roman233233@gmail.com>
2019-07-14 18:39:40 +0100

    no redis :(

--- fetch-stock-data.js
+++ fetch-stock-data.js
@@ -1,16 +1,7 @@
 const fetch = require('node-fetch');
 const qs = require('query-string');
-const redis = require('./redis');
 
 const fetchStockData = async symbol => {
-  const cacheKey = `temp:av:${symbol}`;
-
-  const cached = await redis.get(cacheKey);
-
-  if (cached) {
-    return JSON.parse(cached);
-  }
-
   const query = qs.stringify({
     function: 'TIME_SERIES_DAILY',
     apikey: 'VY93B6B3A6CMLTKC',
@@ -19,11 +10,8 @@ const fetchStockData = async symbol => {
   });
 
   const res = await fetch(`https://www.alphavantage.co/query?${query}`);
-  const fresh = await res.json();
 
-  await redis.set(cacheKey, JSON.stringify(fresh));
-
-  return fresh;
+  return await res.json();
 };
 
 module.exports = fetchStockData;
